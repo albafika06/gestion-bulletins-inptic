4a1703c0f9781acb95b1109cd2fa89dfd28ad70b
hosny minko <hosnyhologram@gmail.com>
2026-04-23 09:44:24 +0200

    Nouveau mot de passe SMTP Gmail

---
+++ test_mail.php
@@ -0,0 +1,6 @@
+﻿<?php
+use Illuminate\Support\Facades\Mail;
+Mail::raw('Test envoi email INPTIC', function($msg) {
+    $msg->to('optisystem01@gmail.com')->subject('Test INPTIC');
+});
+echo 'Email envoye avec succes!';
